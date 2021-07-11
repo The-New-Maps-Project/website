ae345b210c5a805508a0d5f91eb43aa3f13585bd
vincentc48 <vincent.cai48@gmail.com>
2021-07-10 21:32:06 -0400

    footer styles

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,8 @@
+import Link from "next/link";
+
 export default function Footer(){
     return <footer>
         <div>&copy; The New Maps Project 2021</div>
-        <div className="grey-tb ml15">Privacy</div>
+        <Link href="https://thenewmapsproject.org/privacy"><a target="_blank" className="grey-tb ml15">Privacy</a></Link>
     </footer>
 }
