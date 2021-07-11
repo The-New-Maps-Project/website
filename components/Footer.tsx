7661a63c3bc7998daea7ba5037b378ab2a5a8c18
vincentc48 <vincent.cai48@gmail.com>
2021-07-10 21:28:06 -0400

    add footer privacy link

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,3 +1,6 @@
 export default function Footer(){
-    return <footer><div>&copy; The New Maps Project 2021</div></footer>
+    return <footer>
+        <div>&copy; The New Maps Project 2021</div>
+        <div className="grey-tb ml15">Privacy</div>
+    </footer>
 }
