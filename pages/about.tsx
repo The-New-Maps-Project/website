77cfc26543b0ef667c4743b3b00e400243ca51a8
Vincent Cai <vincent.cai48@gmail.com>
2021-08-11 15:20:36 -0400

    add pages

---
+++ pages/about.tsx
@@ -0,0 +1,8 @@
+export default function About(){
+    return <div>
+        <div className="page-header">
+            <h2>About</h2>
+            <p>About The New Map Project</p>
+        </div>
+    </div>
+}
