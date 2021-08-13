eb166312f0da4a329f135d6f3ec606b72cf79959
Vincent Cai <vincent.cai48@gmail.com>
2021-08-13 11:35:03 -0400

    writeNum the district populations

--- components/DistrictsList.tsx
+++ components/DistrictsList.tsx
@@ -1,5 +1,6 @@
 import { useContext } from "react"
 import PContext from "../services/context"
+import writeNum from "../services/writeNum";
 
 export default function DistrictsList(){
     const {districts,districtPops,setViewAloneDistrict,viewAloneDistrict} = useContext(PContext);
@@ -10,7 +11,7 @@ export default function DistrictsList(){
         for(let i:number = 0;i<districts.length;i++){
             arr.push(<li key={i} className={viewAloneDistrict==(i+1)?"selected":""}>
                 <div className="district-name row"><span>District {i+1}</span><span style={{backgroundColor: `var(--${districts[i]}-icon)`}} className="color-box"></span></div>
-                <div className="population">Population: {districtPops[i]}</div>
+                <div className="population">Population: {writeNum(districtPops[i])}</div>
                 {viewAloneDistrict==(i+1)?<button className="unselect" onClick={()=>setViewAloneDistrict(-1)}>Unselect</button>:<button className="view-alone" onClick={()=>setViewAloneDistrict(i+1)}>View Alone</button>}
                 <div className="buttons row">
                     <button className="pack mr15">Pack</button>
