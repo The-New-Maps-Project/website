417d0c9aabd9d83ae3175925a9e0e5ebaf1ea8a2
vincentc48 <vincent.cai48@gmail.com>
2021-09-27 15:46:06 -0400

    datastoreOptions changes

--- services/datastoreOptions.js
+++ services/datastoreOptions.js
@@ -2,8 +2,8 @@ const options = [
     {
         name: "Connecticut",
         districts: "5 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
-        fileName: "ct5zc.txt",
+        info: "10 parameters, Census Tracts as precincts",
+        fileName: "ct5ct.txt",
         zoom: {
             lat: 41.6032, 
             lng: -73.0877,
@@ -14,7 +14,7 @@ const options = [
     {
         name: "North Carolina",
         districts: "14 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "nc14zc.txt",
         zoom: {
             lat: 35.7596, 
@@ -26,19 +26,19 @@ const options = [
     {
         name: "Massachusetts",
         districts: "9 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "ma9zc.txt",
         zoom: {
             lat: 42.4072, 
             lng: -71.3824,
             zoom: 8,
         },
-        runAlgoNotes: "~40 sec; RSD: ~2%"
+        runAlgoNotes: "~40 secs; RSD: ~2%"
     },
     {
         name: "Georgia",
         districts: "14 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "ga14zc.txt",
         zoom: {
             lat: 32.1656, 
@@ -50,7 +50,7 @@ const options = [
     {
         name: "New Jersey",
         districts: "12 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "nj12zc.txt",
         zoom: {
             lat: 40.0583, 
@@ -62,7 +62,7 @@ const options = [
     {
         name: "Colorado",
         districts: "8 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "co8zc.txt",
         zoom: {
             lat: 39.5501, 
@@ -74,19 +74,31 @@ const options = [
     {
         name: "Kentucky",
         districts: "6 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "ky6zc.txt",
         zoom: {
             lat: 37.8393, 
             lng: -84.27,
             zoom: 6,
         },
-        runAlgoNotes: "~30 sec; RSD: ~3%"
+        runAlgoNotes: "~30 secs; RSD: ~3%"
+    },
+    {
+        name: "New Mexico",
+        districts: "3 congressional districts",
+        info: "10 parameters, ZIP codes as precincts",
+        fileName: "nm3zc.txt",
+        zoom: {
+            lat: 34.51, 
+            lng: -105.87,
+            zoom: 6,
+        },
+        runAlgoNotes: "~20 secs; RSD: ~3%"
     },
     {
         name: "Texas",
         districts: "38 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "tx38zc.txt",
         zoom: {
             lat: 31.9686, 
@@ -98,7 +110,7 @@ const options = [
     {
         name: "Florida",
         districts: "28 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "fl28zc.txt",
         zoom: {
             lat: 27.6648, 
@@ -110,7 +122,7 @@ const options = [
     {
         name: "New Hampshire",
         districts: "2 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "nh2zc.txt",
         zoom: {
             lat: 42.19, 
@@ -122,7 +134,7 @@ const options = [
     {
         name: "New York",
         districts: "26 congressional districts",
-        info: "10 parameters, ZIP codes as precicnts",
+        info: "10 parameters, ZIP codes as precincts",
         fileName: "ny26zc.txt",
         zoom: {
             lat: 40.7128, 
